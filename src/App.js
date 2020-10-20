78000d527bbac823b7c58f1c53af5a962bc30185
CathalMoss <g00371316@gmit.ie>
2020-10-20 16:06:50 +0100

    Lab upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import { Content } from './components/content';
+import { Footer } from './components/footer';
+import { Header } from './components/header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+          </Navbar>
+          <br />
+          <Switch>
+            <Route path='/' component={Content} exact />
+            <Route path='/create' component={Header} exact />
+            <Route path='/read' component={Footer} exact />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Footer extends React.Component{
+
+    render(){
+        return(
+        <div>
+        <h1>This is the footer component</h1>
+        </div>
+        );
+    } 
+}
